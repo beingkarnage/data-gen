31f2c05480fbbf2ca656d9d55f0cafcfc6177ef3
Tosif khan <khantosif94@gmail.com>
2023-05-28 21:01:23 +0530

    relation code cleanup

--- relations/relation.py
+++ relations/relation.py
@@ -3,6 +3,7 @@ from utils.strategy_module import load_strategy_module
 def relationType(relation, df, colName,rows, STRATEGIES, LOGICAL_MAPPING):
     filter_dict = relation['filter']
     mask = None
+
     for i in range(len(filter_dict['lhs'])):
         col = filter_dict['lhs'][i]
         value = filter_dict['rhs'][i]
@@ -13,9 +14,10 @@ def relationType(relation, df, colName,rows, STRATEGIES, LOGICAL_MAPPING):
             temp = (df[col] != value) if op == "!=" else (df[col] == value)
             boolean_op = filter_dict['boolean'][i-1]
             mask = mask & temp if boolean_op == "&" else mask | temp
-    # test out    
+
     strategy_module_path = STRATEGIES[relation['strategy']['name']]
     strategy_module = load_strategy_module(strategy_module_path)
     strategy_function = getattr(strategy_module, LOGICAL_MAPPING[relation['strategy']['name']])
-    df = strategy_function(relation['strategy']['params'], df, colName, rows, relation['operation'])
+    df = strategy_function(relation['strategy']['params'],
+                            df, colName, rows, relation['operation'],mask=mask)
     return df
