8c99468f9b68a9d56f1623df7978759dfb4c0467
Tosif khan <khantosif94@gmail.com>
2023-12-30 19:37:41 +0530

    fixed error throwing when a optional config is not given.

--- strategies/date_gen.py
+++ strategies/date_gen.py
@@ -1,19 +1,24 @@
 from utils.date_generator import generate_random_date
 def date_generator_strategy(params, df, colName, rows, operation, mask=True):
-    start_date = params['startDate']
-    end_date = params['endDate']
-    input_format = params['inputFormat']
-    output_format = params['outputFormat']
+    param = {}
+    param["start_date"] = params['startDate']
+    param["end_date"] = params['endDate']
+
+    if "inputFormat" in params :
+        param["input_format"] = params['inputFormat']
+    
+    if 'outputFormat' in params :
+        param["output_format"] = params['outputFormat']
 
     null_mask = mask & df[colName].isnull()
     res = []
     
     if operation == "insert":
         for i in range(rows): 
-            res.append(generate_random_date(start_date, end_date,input_format, output_format))
+            res.append(generate_random_date(**param))
         df[colName] = res
     elif operation == "insertIfEmpty":
         for i in range(null_mask.sum()):
-            res.append(generate_random_date(start_date, end_date,input_format, output_format))
+            res.append(generate_random_date(**param))
         df.loc[null_mask, colName] = res
     return df
