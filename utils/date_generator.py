be457e10d2a8b14cb01b9ba1cb5c11f8040c76a5
Tosif khan <khantosif94@gmail.com>
2023-12-30 19:37:58 +0530

    updated date generation logic

--- utils/date_generator.py
+++ utils/date_generator.py
@@ -1,9 +1,8 @@
 from datetime import datetime, timedelta
+from dateutil import parser
 import random
 def generate_random_date(start_date, end_date, input_format="%d-%m-%Y", output_format="%d-%m-%Y"):
-    start_datetime = datetime.strptime(start_date,input_format )
-    end_datetime = datetime.strptime(end_date, input_format)
-    delta = end_datetime - start_datetime
+    delta = parser.parse(end_date) - parser.parse(start_date)
     random_days = random.randint(0, delta.days)
-    random_date = start_datetime + timedelta(days=random_days)
+    random_date = parser.parse(start_date) + timedelta(days=random_days)
     return random_date.strftime(output_format)
