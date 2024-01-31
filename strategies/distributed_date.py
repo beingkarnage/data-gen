cb335f0f85a0be6feb669ab2268d902818445e65
Tosif khan <khantosif94@gmail.com>
2024-01-31 17:58:24 +0530

    refactored/ typos

--- strategies/distributed_date.py
+++ strategies/distributed_date.py
@@ -4,9 +4,9 @@ from numpy.random import shuffle
 def distributed_date(**kwargs):
     
     df = kwargs.get('df')
-    colName = kwargs.get('col_name')
+    col_name = kwargs.get('col_name')
     rows = kwargs.get('rows')
-    null_mask = kwargs.get('mask',False) & df[colName].isnull()
+    null_mask = kwargs.get('mask',False) & df[col_name].isnull()
 
     params = kwargs.get('params')
     total = 0
@@ -20,13 +20,12 @@ def distributed_date(**kwargs):
     else:
         generated = []
         for i in params:
-            print(i)
             for _ in range(int(i['distribution']/100*rows)):
                 generated.append(generate_random_date(start_date=i['start_date'], end_date=i["end_date"]))
         shuffle(generated)
 
     if kwargs.get('operation') == "insert":
-        df[colName] = generated
+        df[col_name] = generated
     elif kwargs.get('operation') == 'insert_if_empty':
         # df.loc[null_mask, colName] = generated
         print(len(generated),null_mask.sum())
